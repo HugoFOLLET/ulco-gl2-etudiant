be71e3be62c582bc058ad5bd0f130e7b088a0437
TODO <TODO>
2024-02-21 15:41:42 +0100

    TP1 - Organisation d'un projet Python

--- tp-ffi/py-fibo1/mypackage/myfibo.py
+++ tp-ffi/py-fibo1/mypackage/myfibo.py
@@ -1,7 +1,19 @@
 
 def fibo_naive(n: int):
-    assert n >= 0
-    return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
+	assert n >= 0
+	return n if n < 2 else fibo_naive(n-1) + fibo_naive(n-2)
 
 # TODO implement fibo_iterative
+def fibo_iterative(n: int):
+	if n<=0:
+		return 0
+	i = 1
+	k = 0
+	l = 1
+	while i < n:
+		temp = l
+		l = l + k
+		k = temp
+		i=i+1
+	return l
 
--- tp-ffi/py-fibo1/scripts/main.py
+++ tp-ffi/py-fibo1/scripts/main.py
@@ -1,9 +1,19 @@
+from mypackage.myfibo import *
+
 
 if __name__ == '__main__':
 
-    print('TODO')
-    
-    # TODO print fibo_naive(i) for i from 0 to 10
+	print('TODO')
+	
+	# TODO print fibo_naive(i) for i from 0 to 10
+	i = 0
+	while i <= 10:
+		print("fibo_naive("+str(i)+") = "+str(fibo_naive(i)))
+		i=i+1
 
-    # TODO print fibo_iterative(i) for i from 0 to 10
+	# TODO print fibo_iterative(i) for i from 0 to 10
+	i = 0
+	while i <= 10:
+		print("fibo_iterative("+str(i)+") = "+str(fibo_iterative(i)))
+		i=i+1
 
