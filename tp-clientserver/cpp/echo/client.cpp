07e67e91aac9b0ce82afd984a50cd20a45b614c1
TODO <TODO>
2024-04-05 15:38:16 +0200

    Echo

--- tp-clientserver/cpp/echo/client.cpp
+++ tp-clientserver/cpp/echo/client.cpp
@@ -6,34 +6,34 @@
 
 int main(int argc, char** argv) {
 
-    hv::WebSocketClient ws;
-    ws.onopen = []() {
-        std::cout << "connected" << std::endl;
-    };
-    ws.onmessage = [](const std::string& msg) {
-        std::cout << "received: " << msg << std::endl;
-    };
-    ws.onclose = []() {
-        std::cout << "disconnected" << std::endl;
-        exit(0);
-    };
-    ws.open("ws://127.0.0.1:9000");
-    // ws.open("wss://ws.ifelse.io");
+	hv::WebSocketClient ws;
+	ws.onopen = []() {
+		std::cout << "connected" << std::endl;
+	};
+	ws.onmessage = [](const std::string& msg) {
+		std::cout << "received: " << msg << std::endl;
+	};
+	ws.onclose = []() {
+		std::cout << "disconnected" << std::endl;
+		exit(0);
+	};
+	ws.open("ws://127.0.0.1:9000");
+	// ws.open("wss://ws.ifelse.io");
 
-    std::string input;
-    std::cout << "enter messages:" << std::endl;
-    while (true) {
-        std::getline(std::cin, input);
-        
-        if (input == "") {
-            ws.close();
-            break;
-        }
+	std::string input;
+	std::cout << "enter messages:" << std::endl;
+	while (true) {
+		std::getline(std::cin, input);
+		
+		if (input == "") {
+			ws.close();
+			break;
+		}
 
-        ws.send(input);
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
-    }
+		ws.send(input);
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+	}
 
-    return 0;
+	return 0;
 }
 
